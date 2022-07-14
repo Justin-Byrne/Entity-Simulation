2dc66b4d7ab0beb1047b50d329fc949ff27869ec
Justin Don Byrne <justinbyrne001@gmail.com>
2022-07-14 12:27:56 -0700

    general cleanup

--- Entity Simulation/include/config.hpp
+++ Entity Simulation/include/config.hpp
@@ -1,7 +1,7 @@
 // @File:       config.hpp
 // @Project:    Entity Simulation
 // @Author:     Justin Byrne
-// @Date:		Z/reated 6/23/22 at 11:07 AM
+// @Date:		Created 6/23/22 at 11:07 AM
 // @Copyright:	Copyright (c) 2022 Byrne-Systems
 
 #ifndef config_hpp
